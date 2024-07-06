9a9c2e2f07360c5dd247f21acee6b33ea4b92980
mrsterner <simon@sterner.dev>
2024-07-06 13:11:06 +0200

    codec

--- src/main/kotlin/dev/sterner/donkeyhotte/api/recipe/DonkeyProcessingSerializer.kt
+++ src/main/kotlin/dev/sterner/donkeyhotte/api/recipe/DonkeyProcessingSerializer.kt
@@ -3,23 +3,15 @@ package dev.sterner.donkeyhotte.api.recipe
 import com.mojang.serialization.Codec
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
-import io.netty.handler.codec.DecoderException
-import io.netty.handler.codec.EncoderException
-import net.minecraft.core.Holder
-import net.minecraft.core.component.DataComponentPatch
-import net.minecraft.core.component.PatchedDataComponentMap
-import net.minecraft.core.registries.Registries
 import net.minecraft.network.RegistryFriendlyByteBuf
-import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.item.crafting.RecipeSerializer
 
 open class DonkeyProcessingSerializer<T : DonkeyProcessingRecipe>(private val factory: Factory<T>) : RecipeSerializer<T> {
 
-    val strictSingleItemChanceCodec: Codec<ItemStackWithChance> = Codec.lazyInitialized {
+    private val strictSingleItemChanceCodec: Codec<ItemStackWithChance> = Codec.lazyInitialized {
         RecordCodecBuilder.create { instance ->
             instance.group(
                 ItemStack.STRICT_SINGLE_ITEM_CODEC.fieldOf("item").forGetter { it.stack },
@@ -30,7 +22,6 @@ open class DonkeyProcessingSerializer<T : DonkeyProcessingRecipe>(private val fa
 
     private val processingCodec: MapCodec<T> = RecordCodecBuilder.mapCodec { instance ->
         instance.group(
-
             Ingredient.CODEC_NONEMPTY.fieldOf("ingredient").forGetter { it.ingredient },
             strictSingleItemChanceCodec.fieldOf("result").forGetter { it.output },
             strictSingleItemChanceCodec.fieldOf("extra").forGetter { it.extraOutput },
@@ -41,27 +32,37 @@ open class DonkeyProcessingSerializer<T : DonkeyProcessingRecipe>(private val fa
     private val streamCodec: StreamCodec<RegistryFriendlyByteBuf, T> = StreamCodec.of(::toNetwork, ::fromNetwork)
 
     private fun fromNetwork(registryFriendlyByteBuf: RegistryFriendlyByteBuf): T {
+        //Input
         val inputIngredient = Ingredient.CONTENTS_STREAM_CODEC.decode(registryFriendlyByteBuf)
+
+        //Output with chance
         val outputStack = ItemStack.STREAM_CODEC.decode(registryFriendlyByteBuf)
         val outputChance = registryFriendlyByteBuf.readFloat()
 
+        //Extra output with chance
         val extraOutputStack = ItemStack.STREAM_CODEC.decode(registryFriendlyByteBuf)
         val extraOutputChance = registryFriendlyByteBuf.readFloat()
 
+        //Processing time
         val processingTime = registryFriendlyByteBuf.readVarInt()
+
         return factory.create(inputIngredient, ItemStackWithChance(outputStack, outputChance), ItemStackWithChance(extraOutputStack, extraOutputChance), processingTime)
     }
 
 
     private fun toNetwork(registryFriendlyByteBuf: RegistryFriendlyByteBuf, grindstoneRecipe: DonkeyProcessingRecipe) {
+        //Input
         Ingredient.CONTENTS_STREAM_CODEC.encode(registryFriendlyByteBuf, grindstoneRecipe.ingredient)
 
+        //Output with chance
         ItemStack.STREAM_CODEC.encode(registryFriendlyByteBuf, grindstoneRecipe.output.stack)
         registryFriendlyByteBuf.writeFloat(grindstoneRecipe.output.chance)
 
+        //Extra output with chance
         ItemStack.STREAM_CODEC.encode(registryFriendlyByteBuf, grindstoneRecipe.extraOutput.stack)
         registryFriendlyByteBuf.writeFloat(grindstoneRecipe.extraOutput.chance)
 
+        //Processing time
         registryFriendlyByteBuf.writeVarInt(grindstoneRecipe.processingTime)
     }
 
