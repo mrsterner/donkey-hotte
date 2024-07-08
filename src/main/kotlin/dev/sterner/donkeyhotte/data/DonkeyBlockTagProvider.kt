76edb85b3515f8c46705a4eea83edd734a97c4f0
mrsterner <simon@sterner.dev>
2024-07-08 15:05:31 +0200

    fix broken

--- src/main/kotlin/dev/sterner/donkeyhotte/data/DonkeyBlockTagProvider.kt
+++ src/main/kotlin/dev/sterner/donkeyhotte/data/DonkeyBlockTagProvider.kt
@@ -4,10 +4,7 @@ import dev.sterner.donkeyhotte.registry.DonkeyBlocks
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
 import net.minecraft.core.HolderLookup
-import net.minecraft.core.Registry
-import net.minecraft.resources.ResourceKey
 import net.minecraft.tags.BlockTags
-import net.minecraft.world.level.block.Block
 import java.util.concurrent.CompletableFuture
 
 class DonkeyBlockTagProvider(output: FabricDataOutput?,
